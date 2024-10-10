82ca2111d5421811cd5ff3f611c042bd552dd2fe
hansol <hskang@rainbow-robotics.com>
2024-10-10 11:40:53 +0900

    update

--- assets/README.md.c7z1Lbu1.js
+++
@@ -1,8 +0,0 @@
-import{_ as e,c as s,a1 as o,o as t}from"./chunks/framework.Bff2UY46.js";const v=JSON.parse('{"title":"Doc proposal","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),p={name:"README.md"};function n(d,a,c,l,r,i){return t(),s("div",null,a[0]||(a[0]=[o(`<h1 id="doc-proposal" tabindex="-1">Doc proposal <a class="header-anchor" href="#doc-proposal" aria-label="Permalink to &quot;Doc proposal&quot;">​</a></h1><div class="language-bash vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre><code>npm install
-</code></pre></div><div class="language-bash vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre><code>npm run docs:dev
-# or
-# npx vitepress dev
-</code></pre></div><p>deploy</p><div class="language-bash vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre><code>npm run docs:build
-cd .vitepress/dist
-git push
-</code></pre></div>`,5)]))}const h=e(p,[["render",n]]);export{v as __pageData,h as default};
